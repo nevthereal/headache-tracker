13b5f02c2f68b9c0821411d5cb3b13649c6d15e6
Neville Brem <contact@nevillebrem.com>
2025-04-18 19:59:28 +0200

    trpc stuff to load entries

--- apps/server/src/lib/context.ts
+++ apps/server/src/lib/context.ts
@@ -1,17 +1,19 @@
 import type { Context as HonoContext } from "hono";
 import { auth } from "./auth";
+import { db } from "../db";
 
 export type CreateContextOptions = {
-	context: HonoContext;
+  context: HonoContext;
 };
 
 export async function createContext({ context }: CreateContextOptions) {
-	const session = await auth.api.getSession({
-		headers: context.req.raw.headers,
-	});
-	return {
-		session,
-	};
+  const session = await auth.api.getSession({
+    headers: context.req.raw.headers,
+  });
+  return {
+    session,
+    db,
+  };
 }
 
 export type Context = Awaited<ReturnType<typeof createContext>>;
--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,13 +1,27 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { useQuery } from "@tanstack/react-query";
+import { trpc } from "@/utils/trpc";
 
 export const Route = createFileRoute("/")({
   component: HomeComponent,
 });
 
 function HomeComponent() {
+  const entries = useQuery(trpc.entries.queryOptions());
+
   return (
     <div className='container mx-auto max-w-3xl px-4 py-2'>
       <h1 className='font-bold text-2xl'>Letzte Einträge:</h1>
+
+      {entries.isLoading ? (
+        <p>Loading...</p>
+      ) : entries.data ? (
+        <ul>
+          {entries.data?.map((entry) => <li key={entry.id}>{entry.title}</li>)}
+        </ul>
+      ) : (
+        <p>No entries found</p>
+      )}
     </div>
   );
 }
